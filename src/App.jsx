3692a29676af6f052240c23b5291c467eac2fe4c
Chay <chaymaeelha@gmail.com>
2024-03-28 23:11:30 +0100

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
 import './App.css';
 import PokemonCard from './components/PokemonCard';
+import { useState } from "react";
 
 function App() {
-  return (<div>
-<PokemonCard pokemon={pokemonList[0]}/>
-  </div>
-  );
-}
-const pokemonList = [
+  const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    name: "Bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "mew",
+    name: "Charmander",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "Squirtle",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "Pikachu",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "Mew",
   },
 ];
+  
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
+  const handleClickForward = () => {
+    setPokemonIndex(pokemonIndex + 1)
+  }
+
+  const handleClickBackward = () => {
+    setPokemonIndex(pokemonIndex - 1)
+}
+  return (
+  <div>
+    <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+    <div>{pokemonIndex > 0 ? <button onClick={handleClickBackward}>Précédent</button> : null}
+    {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClickForward}>Suivant</button> : null}</div>
+  </div>
+  );
+  }
 export default App
